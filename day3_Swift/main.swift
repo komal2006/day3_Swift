8aae37d43b73b3aa1cd28add162d367589489aaa
Komal2006 <komaldeepkr99@gmail.com>
2019-10-11 18:02:54 -0400

    Initial Commit

---
+++ day3_Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  day3_Swift
+//
+//  Created by MacStudent on 2019-10-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
